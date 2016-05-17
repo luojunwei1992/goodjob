09a3a9e55c70ce2717e336371e7c4b3a59a6e47e
罗俊威 <569293064@qq.com>
2016-05-17 10:20:06 +0800

    基本框架搭建

--- ljwb/ljwb/Classes/Main/MainTabBarController.swift
+++ ljwb/ljwb/Classes/Main/MainTabBarController.swift
@@ -12,24 +12,36 @@ class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addChildViewControllers()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func addChildViewControllers(){
+    
+        
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
     }
-    */
+    
+    
+    private func addChildViewController(childController: UIViewController, title : String, imageName:String) {
+        
+        childController.title = title
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        
+        tabBar.tintColor = UIColor.orangeColor();
+        
+        let nav = UINavigationController()
+        
+        nav.addChildViewController(childController)
+        
+        addChildViewController(nav)
+    }
+    
 
 }
